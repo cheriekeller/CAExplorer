7a1559576fee84fdcb8d0d9d22790e98e585c4d6
Brendan Ward <bcward@consbio.org>
2019-03-23 20:26:52 -0700

    Fix for HTTPS basemaps

--- src/components/Map/index.jsx
+++ src/components/Map/index.jsx
@@ -119,7 +119,7 @@ const config = {
       }
     ),
     L.tileLayer(
-      'http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+      '//server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
       {
         attribution:
           'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community',
