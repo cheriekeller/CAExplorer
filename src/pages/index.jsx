95e51a51b1ed0f77a02aeb8d9fb393beb8111581
Brendan Ward <bcward@consbio.org>
2019-04-10 08:42:12 -0700

    Home page updates

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -6,7 +6,6 @@ import { FaQuestionCircle } from 'react-icons/fa'
 
 import { Flex, Box, Container } from 'components/Grid'
 import { Link } from 'components/Link'
-import Divider from 'components/elements/Divider'
 import Layout from 'components/layout/Home'
 import SEO from 'components/SEO'
 import Thumbnail from 'components/Profile/Thumbnail'
@@ -18,15 +17,25 @@ import FWCLogo from '../images/fwc_logo.png'
 import PFLCCLogo from '../images/pflcc_logo.png'
 
 const PullQuote = styled.p`
-  margin: 1rem 2rem;
+  margin: 3rem 2rem 0;
   color: ${themeGet('colors.grey.800')};
   font-size: 1.2em;
   font-style: italic;
 `
 
+const InvertedHeader = styled.h1`
+  background-color: ${themeGet('colors.primary.900')};
+  color: ${themeGet('colors.white')};
+  padding: 2rem;
+  margin: 4rem 0 2rem;
+  text-align: center;
+  border-radius: 1rem;
+`
+
 const Section = styled.section`
+  margin-top: 2rem;
+
   &:not(:first-child) {
-    margin-top: 2rem;
     padding-top: 2rem;
     border-top: 1px solid ${themeGet('colors.grey.200')};
   }
@@ -37,6 +46,7 @@ const SectionHeader = styled.h3``
 const SectionBody = styled(Flex).attrs({ flexWrap: 'wrap' })`
   p {
     flex: 1;
+    min-width: 200px;
   }
 `
 
@@ -46,24 +56,38 @@ const StyledThumbnail = styled(Thumbnail)`
 `
 
 const HelpSection = styled(Box)`
+  margin: 6rem 0 4rem;
   padding: 1rem;
   border-radius: 1rem;
   background-color: ${themeGet('colors.primary.900')};
   color: ${themeGet('colors.white')};
 
-  h3 {
+  h2 {
     text-align: center;
     color: ${themeGet('colors.white')};
+  }
+  a {
+    color: ${themeGet('colors.white')};
+    text-decoration: underline;
   }
 `
 const HelpIcon = styled(FaQuestionCircle)`
   height: 4rem;
   width: 4rem;
   color: ${themeGet('colors.white')};
-  margin: 0 1rem;
+  margin: 0 2rem;
 `
 
-const IndexPage = ({ data: { headerImage, bodyImage1 } }) => (
+const CreditsSection = styled.section`
+  font-size: 0.9em;
+  color: ${themeGet('colors.grey.600')};
+
+  h3 {
+    text-align: center;
+  }
+`
+
+const IndexPage = ({ data: { headerImage } }) => (
   <Layout>
     <SEO />
 
@@ -111,104 +135,98 @@ const IndexPage = ({ data: { headerImage, bodyImage1 } }) => (
         on Florida’s ecosystems.
       </p>
 
-      <Section>
-        <SectionHeader>
-          <Link to="/impacts">Climate Impacts in Florida</Link>
-        </SectionHeader>
-        <SectionBody>
-          <StyledThumbnail id="118" />
-          <p>
-            Florida is expected to be more severely impacted by climate change
-            over the next century that most other states in the U.S. Many
-            species of fish and wildlife in Florida have limited ranges that are
-            determined in part by climatic conditions and their survival could
-            be threatened by future climatic shifts.
-            <br />
-            <br />
-            Learn more about climate change, climate impacts to Florida&apos;s
-            unique species and habitats, and more.{' '}
-            <Link to="/impacts">Read more...</Link>
-          </p>
-        </SectionBody>
-      </Section>
+      <InvertedHeader>Use this tool to learn more about:</InvertedHeader>
 
-      <Section>
-        <SectionHeader>
-          <Link to="/species">Climate Impacts on Florida&apos;s Species</Link>
-        </SectionHeader>
-        <SectionBody>
-          <StyledThumbnail id="217" />
-          <p>
-            <b>
-              Climate change is expected to have profound impacts on
-              Florida&apos;s species.
-            </b>
-            <br />
-            Nearly one quarter of the approximately 1,200 species tracked by the
-            Florida Natural Areas Inventory are projected to have at least 50%
-            of their range lost to a sea level rise of 1 m. Several keystone
-            species are particularly vulnerable to the impacts of climate change
-            and the loss of these species can have cascading impacts on natural
-            communities and other species.
-            <br />
-            <br />
-            Learn more about climate impacts for 138 species of concern in
-            Florida. Each species has a detailed profile that includes
-            information on life history, range, habitat use, climate change
-            impacts, vulnerability, and adaptation strategies.{' '}
-            <Link to="/species">Start exploring species profiles...</Link>
-          </p>
-        </SectionBody>
-      </Section>
+      <div>
+        <Section>
+          <SectionHeader>
+            <Link to="/impacts">Climate Impacts in Florida</Link>
+          </SectionHeader>
+          <SectionBody>
+            <StyledThumbnail id="118" />
+            <p>
+              Florida is expected to be more severely impacted by climate change
+              over the next century that most other states in the U.S. Many
+              species of fish and wildlife in Florida have limited ranges that
+              are determined in part by climatic conditions and their survival
+              could be threatened by future climatic shifts.
+              <br />
+              <br />
+              Learn more about climate change, climate impacts to Florida&apos;s
+              unique species and habitats, and more.{' '}
+              <Link to="/impacts">Read more...</Link>
+            </p>
+          </SectionBody>
+        </Section>
 
-      <Section>
-        <SectionHeader>
-          <Link to="/habitats">
-            Climate Impacts on Florida&apos;s Ecosystems and Habitats
-          </Link>
-        </SectionHeader>
-        <SectionBody>
-          <StyledThumbnail id="1310" />
-          <p>
-            Explore detailed profiles for 50 ecosystems, conservation assets,
-            and habitats across Florida. Each habitat profile includes
-            information on the location and defining characteristics of each
-            habitat, climate impacts, vulnerability, and adaptation strategies.{' '}
-            <Link to="/habitats">Start exploring habitat profiles...</Link>
-          </p>
-        </SectionBody>
-      </Section>
+        <Section>
+          <SectionHeader>
+            <Link to="/species">Climate Impacts on Florida&apos;s Species</Link>
+          </SectionHeader>
+          <SectionBody>
+            <StyledThumbnail id="217" />
+            <p>
+              <b>
+                Climate change is expected to have profound impacts on
+                Florida&apos;s species.
+              </b>
+              <br />
+              Nearly one quarter of the approximately 1,200 species tracked by
+              the Florida Natural Areas Inventory are projected to have at least
+              50% of their range lost to a sea level rise of 1 m. Several
+              keystone species are particularly vulnerable to the impacts of
+              climate change and the loss of these species can have cascading
+              impacts on natural communities and other species.
+              <br />
+              <br />
+              Learn more about climate impacts for 138 species of concern in
+              Florida. Each species has a detailed profile that includes
+              information on life history, range, habitat use, climate change
+              impacts, vulnerability, and adaptation strategies.{' '}
+              <Link to="/species">Start exploring species profiles...</Link>
+            </p>
+          </SectionBody>
+        </Section>
 
-      <Section>
-        <SectionHeader>
-          <Link to="/strategies">Climate Adaptation Strategies</Link>
-        </SectionHeader>
-        <SectionBody>
-          <StyledThumbnail id="1310" />
-          <p>
-            Find out what you can do to help plan for the future survival of
-            Florida&apos;s unique species and habitats. Using an adaptation
-            approach is vital to offset the increasing impacts from climate
-            change combined with existing anthropogenic impacts to natural
-            communities and species.{' '}
-            <Link to="/strategies">Learn what you can do...</Link>
-          </p>
-        </SectionBody>
-      </Section>
+        <Section>
+          <SectionHeader>
+            <Link to="/habitats">
+              Climate Impacts on Florida&apos;s Ecosystems and Habitats
+            </Link>
+          </SectionHeader>
+          <SectionBody>
+            <StyledThumbnail id="1310" />
+            <p>
+              Explore detailed profiles for 50 ecosystems, conservation assets,
+              and habitats across Florida. Each habitat profile includes
+              information on the location and defining characteristics of each
+              habitat, climate impacts, vulnerability, and adaptation
+              strategies.{' '}
+              <Link to="/habitats">Start exploring habitat profiles...</Link>
+            </p>
+          </SectionBody>
+        </Section>
 
-      {/* <FluidImage
-        image={bodyImage1.childImageSharp.fluid}
-        height="40vh"
-        minHeight="20rem"
-        credits={{
-          url:
-            'https://www.flickr.com/photos/evergladesnps/9101434468/in/album-72157626553127734/',
-          author: 'G.Gardner (NPS)',
-        }}
-      /> */}
+        <Section>
+          <SectionHeader>
+            <Link to="/strategies">Climate Adaptation Strategies</Link>
+          </SectionHeader>
+          <SectionBody>
+            <StyledThumbnail id="328" />
+            <p>
+              Find out what you can do to help plan for the future survival of
+              Florida&apos;s unique species and habitats. Using an adaptation
+              approach is vital to offset the increasing impacts from climate
+              change combined with existing anthropogenic impacts to natural
+              communities and species.{' '}
+              <Link to="/strategies">Learn what you can do...</Link>
+            </p>
+          </SectionBody>
+        </Section>
+      </div>
 
-      <HelpSection m={['1rem', '2rem', '6rem']}>
-        <SectionHeader>Need help?</SectionHeader>
+      <HelpSection>
+        <SectionHeader as="h2">Need help?</SectionHeader>
         <SectionBody justifyContent="space-between">
           <HelpIcon />
           <p>
@@ -223,67 +241,83 @@ const IndexPage = ({ data: { headerImage, bodyImage1 } }) => (
         </SectionBody>
       </HelpSection>
 
-      <h3>Credits</h3>
-      <p>
-        Much of the content presented here is taken from{' '}
-        <a
-          href="https://myfwc.com/media/5864/adaptation-guide.pdf"
-          target="_blank"
-          rel="noopener noreferrer"
+      <CreditsSection>
+        <h3>Credits</h3>
+        <p>
+          Much of the content presented here is taken from{' '}
+          <a
+            href="https://myfwc.com/media/5864/adaptation-guide.pdf"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            A Guide to Climate Change Adaptation for Conservation: Resources and
+            Tools for Climate Smart Management of Florida&apos;s Fish and
+            Wildlife Species and Their Habitats
+          </a>{' '}
+          (Florida Fish and Wildlife Conservation Commission, 2016) and the{' '}
+          <a
+            href="http://floridaclimateinstitute.org/docs/climatebook/Ch12-Stys.pdf"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Climate Change Impacts on Florida&apos;s Biodiversity and
+            Ecology&quot; chapter in &quot;Florida’s Climate: Changes,
+            Variations and Impacts
+          </a>{' '}
+          (Florida Climate Institute, 2017)
+        </p>
+        <p>
+          This application was created by{' '}
+          <a href="mailto:bcward@consbio.org">Brendan C. Ward</a> at the&nbsp;
+          <a
+            href="https://consbio.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Conservation Biology Institute
+          </a>
+          &nbsp; (CBI) in partnership with{' '}
+          <a href="mailto:beth.stys@myfwc.com">Beth Stys</a>,{' '}
+          <a href="mailto:Lily.SwanbrowBecker@myfwc.com">
+            Lily Swanbrow Becker
+          </a>
+          , and <a href="mailto:Cherie.Keller@myfwc.com">Cherie Keller</a> with
+          the{' '}
+          <a
+            href="http://peninsularfloridalcc.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Peninsular Florida Landscape Conservation Cooperative
+          </a>{' '}
+          /{' '}
+          <a
+            href="https://myfwc.com/conservation/special-initiatives/climate-change/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Florida Fish and Wildlife Conservation Commission
+          </a>
+          .
+        </p>
+        <Flex
+          justifyContent="space-between"
+          alignItems="center"
+          flexWrap="wrap"
         >
-          A Guide to Climate Change Adaptation for Conservation: Resources and
-          Tools for Climate Smart Management of Florida&apos;s Fish and Wildlife
-          Species and Their Habitats
-        </a>{' '}
-        (Florida Fish and Wildlife Conservation Commission, 2016) and the{' '}
-        <a
-          href="http://floridaclimateinstitute.org/docs/climatebook/Ch12-Stys.pdf"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Climate Change Impacts on Florida&apos;s Biodiversity and
-          Ecology&quot; chapter in &quot;Florida’s Climate: Changes, Variations
-          and Impacts
-        </a>{' '}
-        (Florida Climate Institute, 2017)
-      </p>
-      <p>
-        This application was created by{' '}
-        <a href="mailto:bcward@consbio.org">Brendan C. Ward</a> at the&nbsp;
-        <a href="https://consbio.org" target="_blank" rel="noopener noreferrer">
-          Conservation Biology Institute
-        </a>
-        &nbsp; (CBI) in partnership with{' '}
-        <a href="mailto:beth.stys@myfwc.com">Beth Stys</a>,{' '}
-        <a href="mailto:Lily.SwanbrowBecker@myfwc.com">Lily Swanbrow Becker</a>,
-        and <a href="mailto:Cherie.Keller@myfwc.com">Cherie Keller</a> with the{' '}
-        <a
-          href="http://peninsularfloridalcc.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Peninsular Florida Landscape Conservation Cooperative
-        </a>{' '}
-        /{' '}
-        <a
-          href="https://myfwc.com/conservation/special-initiatives/climate-change/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Florida Fish and Wildlife Conservation Commission
-        </a>
-        .
-      </p>
-      <Flex justifyContent="space-between" alignItems="center" flexWrap="wrap">
-        <Image src={CBILogo} alt="CBI Logo" height={['40px', '50px', '64px']} />
-        <Image src={FWCLogo} alt="FWC Logo" height={['70px', '100px']} />
-        <Image
-          src={PFLCCLogo}
-          alt="PFLCC Logo"
-          height={['40px', '50px', '64px']}
-        />
-      </Flex>
-      <p />
+          <Image
+            src={CBILogo}
+            alt="CBI Logo"
+            height={['40px', '50px', '64px']}
+          />
+          <Image src={FWCLogo} alt="FWC Logo" height={['70px', '100px']} />
+          <Image
+            src={PFLCCLogo}
+            alt="PFLCC Logo"
+            height={['40px', '50px', '64px']}
+          />
+        </Flex>
+      </CreditsSection>
     </Container>
   </Layout>
 )
@@ -302,14 +336,6 @@ export const pageQuery = graphql`
     headerImage: file(
       relativePath: { eq: "ray-hennessy-1134702-unsplash.jpg" }
     ) {
-      childImageSharp {
-        fluid(maxWidth: 4160) {
-          ...GatsbyImageSharpFluid_withWebp
-        }
-      }
-    }
-
-    bodyImage1: file(relativePath: { eq: "9101434468_3d0807e732_o.jpg" }) {
       childImageSharp {
         fluid(maxWidth: 4160) {
           ...GatsbyImageSharpFluid_withWebp
