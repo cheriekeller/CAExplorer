aa9966e114a6d41b0d85a1e17871995ee056bcd7
Brendan Ward <bcward@consbio.org>
2019-05-21 18:20:18 -0700

    Fix display issue on Firefox

--- src/components/layout/Default.jsx
+++ src/components/layout/Default.jsx
@@ -17,6 +17,7 @@ const Wrapper = styled(Flex).attrs({ flexDirection: 'column' })`
 
 const PageContent = styled(Flex)`
   height: 100%;
+  overflow-y: auto;
 `
 
 const SidebarToggle = styled.button`
